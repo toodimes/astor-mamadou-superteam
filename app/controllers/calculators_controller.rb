2766b594aba734ba885751264b5efdcda161941b
David Astor <toodimes@gmail.com>
2017-07-06 20:55:27 -0400

    modify calc

--- app/controllers/calculators_controller.rb
+++ app/controllers/calculators_controller.rb
@@ -8,6 +8,14 @@ class CalculatorsController < ApplicationController
   end
 
   def subtract
+    if params[:number_one].to_i > params[:number_two].to_i
+      @answer = params[:number_one].to_i - params[:number_two].to_i
+    elsif params[:number_one].to_i < params[:number_two].to_i
+      @answer = params[:number_two].to_i - params[:number_one].to_i
+    else
+      @answer = params[:number_one].to_i - params[:number_two].to_i
+    end
+    render :answer
   end
 
 end
